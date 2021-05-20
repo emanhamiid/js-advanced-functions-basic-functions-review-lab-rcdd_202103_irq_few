3f90bf4de395d0912b987064c8a34167f43662f5
eman hamid <emanhharki@gmail.com>
2021-05-20 12:12:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code here
+const saturdayfun = function saturdayFun(a, b) {
+  return  "This Saturday, I want to roller-skate!";
+  return "This Saturday, I want to bathe my dog!";
+}
+saturdayFun();
